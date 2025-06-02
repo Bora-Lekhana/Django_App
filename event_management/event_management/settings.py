63b008c1016eec77e5b33728551ceb63bea3051a
Bora-Lekhana <lekhanasairam01@gmail.com>
2025-06-02 18:46:17 +0530

    Prepare app for Render deployment

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(2r2$$xc&i4_+(^o7(lm657ya%%l22w%02=lngmskk46wgr=k#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
